1796393a55d1d1e7766a634d862b5b1f8db3531f
darshanasam1234 <64640091+darshanasam1234@users.noreply.github.com>
2023-09-04 15:40:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    return 'Hello, Azure App Service!'
 
 if __name__ == '__main__':
     app.run(debug=True)
